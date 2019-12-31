be1c2676e2ea038e145d82a823885d20a8bc17f9
Emanuel de Jong <edejong01@student.rocvantwente.nl>
2019-12-31 14:11:46 +0100

    form validation done

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+function uploadedFileIsMidi() {
+    if(!$("#midi").val()){
+        return false;
+    }
+
+    if ($("#midi").val().slice(-3) == 'mid') {
+        return true;
+    } else {
+        return false;
+    }
+}
